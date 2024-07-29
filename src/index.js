98f1c8ebb0d9bb0789753e0ae083bf43dbf501b2
rushikesherk <rushikesh.sasamkar@eronkan.com>
2024-07-29 23:06:49 +0530

    Connected to the mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv';
+import express from "express"
+import connectDB from "./db/index.js";
+const app  = express();
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async()=>{
+    try {
+
+        await mongoose.connect(`{process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("Error", error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App running on port ${process.env.PORT}`);
+        })
+        
+    } catch (error) {
+        console.error(error)
+    }
+})()*/
